f93af34ce1a064b3facd1a2b7d3275a14a12fcf7
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-22 15:48:48 -0300

    33- functions

--- 02-go-essentials/investment_calculator.go
+++ 02-go-essentials/investment_calculator.go
@@ -11,13 +11,16 @@ func main() {
 	var years float64
 	expectedReturnRate := 5.5
 
-	fmt.Print("Investment Amount: ")
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
+	// fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	// fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
@@ -31,4 +34,8 @@ func main() {
 	// fmt.Printf(`Future Value: %.1f
 	// Future Value (adjusted for Inflation):%.1f`, futureValue, futureRealValue)
 	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
