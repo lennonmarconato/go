41d157573305c50a7d1b1bff0551090349ae96f4
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-22 20:27:14 -0300

    52- exercise

--- 02-go-essentials/27-exercise-profit-calculator/profit_calculator.go
+++ 02-go-essentials/27-exercise-profit-calculator/profit_calculator.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
@@ -14,18 +35,28 @@ func main() {
 	fmt.Printf("Ratio: %.3f\n", ratio)
 }
 
+func storeResult(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
 func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expenses
 	profit = ebt * (1 - taxRate/100)
 	ratio = ebt / profit
+	storeResult(ebt, profit, ratio)
 
 	return
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value must be a positive number.")
+	}
+
+	return userInput, nil
 }
