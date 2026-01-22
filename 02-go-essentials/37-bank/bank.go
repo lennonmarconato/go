18aab0a26a380ba4045b16ad9e2fae9493352aaa
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-22 19:29:26 -0300

    46- switch

--- 02-go-essentials/37-bank/bank.go
+++ 02-go-essentials/37-bank/bank.go
@@ -20,9 +20,10 @@ func main() {
 
 		// wantsCheckBalance := choice == 1
 
-		if choice == 1 {
+		switch choice {
+		case 1:
 			fmt.Println("Your balance is", accountBalance)
-		} else if choice == 2 {
+		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
@@ -34,7 +35,7 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-		} else if choice == 3 {
+		case 3:
 			fmt.Print("Your Withdrawal  amount: ")
 			var withdrawalAmount float64
 			fmt.Scan(&withdrawalAmount)
@@ -51,11 +52,12 @@ func main() {
 
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-		} else {
+		default:
 			fmt.Println("Goodbye!")
-			break
+			fmt.Println("Thanks for choosing our bank")
+			return
+			// break
 		}
 	}
 
-	fmt.Println("Thanks for choosing our bank")
 }
