dd8903966abf9c6c2e1541e9e973b26c2c5c1f56
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-22 14:07:45 -0300

    22- alternative variable declaration

--- 02-go-essentials/investment_calculator.go
+++ 02-go-essentials/investment_calculator.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	// var investmentAmount, years = 1000, "10"
+	// var investmentAmount, years float64 = 1000, 10
+	// investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
 }
