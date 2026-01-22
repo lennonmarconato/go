c348254f88cf3d7bf3ccac209072b5b8935efeae
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-22 15:28:58 -0300

    31- creating formatted strings

--- 02-go-essentials/investment_calculator.go
+++ 02-go-essentials/investment_calculator.go
@@ -23,8 +23,11 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation):%.1f\n", futureRealValue)
 	// outputs information
 	// fmt.Println("Future Value:", futureValue)
 	// fmt.Println("Future Value (adjusted for Inflation):", futureRealValue)
-	fmt.Printf("Future Value: %.1f\nFuture Value (adjusted for Inflation):%.1f", futureValue, futureRealValue)
+	// fmt.Printf("Future Value: %.1f\nFuture Value (adjusted for Inflation):%.1f", futureValue, futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
 }
