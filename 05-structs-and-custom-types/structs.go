c5875efa587f1e4e2193d731247edef9054b119f
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-23 13:47:44 -0300

    81- introducing methods

--- 05-structs-and-custom-types/structs.go
+++ 05-structs-and-custom-types/structs.go
@@ -12,6 +12,10 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -26,12 +30,7 @@ func main() {
 	}
 
 	// ... do something awesome with that gathered data!
-	outputUserDetails(&appUser)
-
-}
-
-func outputUserDetails(u *user) {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
