e859e7cba87f00470e0049e8001dd0bdc48e7388
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-26 16:22:30 -0300

    93- handling long user input text

--- 05-structs-and-custom-types/structs-pratice/main.go
+++ 05-structs-and-custom-types/structs-pratice/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"example.com/note/note"
 )
@@ -27,9 +30,18 @@ func getNoteData() (string, string) {
 }
 
 func getUserInput(prompt string) string {
-	fmt.Print(prompt)
-	var value string
-	fmt.Scan(&value)
+	fmt.Printf("%v ", prompt)
 
-	return value
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
