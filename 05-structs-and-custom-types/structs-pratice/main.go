ae1bb5f3faecb08c99dd74e73fb41cb09e799782
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-26 13:20:57 -0300

    91- creating a struct & constructor

--- 05-structs-and-custom-types/structs-pratice/main.go
+++ 05-structs-and-custom-types/structs-pratice/main.go
@@ -1,45 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"example.com/note/note"
 )
 
 func main() {
-	title, content, err := getNoteData()
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(title, content)
+	fmt.Println(userNote)
 }
 
-func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note title:")
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
 
-	if err != nil {
-		return "", "", err
-	}
-
-	content, err := getUserInput("Note content:")
-
-	if err != nil {
-		return "", "", err
-	}
-
-	return title, content, nil
+	return title, content
 }
 
-func getUserInput(prompt string) (string, error) {
+func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	var value string
 	fmt.Scan(&value)
 
-	if value == "" {
-		return "", errors.New("Invalid input.")
-	}
-
-	return value, nil
+	return value
 }
