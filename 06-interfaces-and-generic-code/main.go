97cc5ac6c54e4a5dfc74e4fd46baff1a2b2868cb
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-27 10:24:02 -0300

    100- using 'todo'

--- 06-interfaces-and-generic-code/main.go
+++ 06-interfaces-and-generic-code/main.go
@@ -7,10 +7,19 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text:")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -18,6 +27,16 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	todo.Display()
+	err = todo.Save()
+
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return
+	}
+
+	fmt.Println("Saving the todo succeeded!")
 
 	userNote.Display()
 	err = userNote.Save()
