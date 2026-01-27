2dfdf5c023b1c81468211d47a5bf4306cceb896a
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-27 16:13:47 -0300

    124- working with type aliases

--- 07-arrays-slices-maps/main.go
+++ 07-arrays-slices-maps/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	userNames := make([]string, 2, 5)
 
@@ -12,11 +18,12 @@ func main() {
 
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
+	// fmt.Println(courseRatings)
 }
