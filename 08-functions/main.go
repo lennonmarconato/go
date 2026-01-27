7b108f020704d6c9cec01f1cdd26b322b3af5cb2
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-27 18:58:11 -0300

    133- spliting slices into parameter values

--- 08-functions/main.go
+++ 08-functions/main.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
-	// numbers := []int{1, 10, 15}
+	numbers := []int{1, 10, 15}
 	sum := sumup(1, 10, 15, 40, -5)
+	anotherSum := sumup(numbers...)
 
 	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
 func sumup(numbers ...int) int {
