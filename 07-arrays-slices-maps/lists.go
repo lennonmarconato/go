aaef98f33c6ac5e2aa8fec568d1d3919dcf5c867
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-27 15:35:35 -0300

    118- unpacking list values

--- 07-arrays-slices-maps/lists.go
+++ 07-arrays-slices-maps/lists.go
@@ -7,8 +7,12 @@ func main() {
 	fmt.Println(prices[0:1])
 	prices[1] = 9.99
 
-	prices = append(prices, 5.99)
+	prices = append(prices, 5.99, 12.99, 29.99, 100.10)
 	prices = prices[1:]
+	fmt.Println(prices)
+
+	discontPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discontPrices...)
 	fmt.Println(prices)
 }
 
