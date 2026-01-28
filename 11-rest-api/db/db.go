c5cddbd349185dd6938057db732ba5843963e822
Lennon de Sousa Marconato <lennon_marconato@hotmail.com>
2026-01-28 19:53:56 -0300

    169- creating a sql database table & fix sqlite

--- 11-rest-api/db/db.go
+++ 11-rest-api/db/db.go
@@ -3,14 +3,14 @@ package db
 import (
 	"database/sql"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var er error
-	DB, er = sql.Open("sqlite3", "api.db")
+	DB, er = sql.Open("sqlite", "api.db")
 
 	if er != nil {
 		panic("Could not connect to database.")
@@ -18,4 +18,25 @@ func InitDB() {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER NOT NULL
+	)
+	`
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
+
 }
